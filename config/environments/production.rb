0945a804cfe2620eaeb4a3306d0d61d616ed7099
Igor Rudym <irudym@gmail.com>
2020-04-26 20:52:56 +0300

    ENV fix
    [=] changed doulbe quote to single quote

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # Use Mailgun for mails
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: ENV["MAILGUN-API-KEY"],
+    api_key: ENV['MAILGUN_API_KEY'],
     domain: 'mg.ilo-ilo.me',
     api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
   }
